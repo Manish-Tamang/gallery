f4c14cdce775078cb1f68e9ff021d24d78b085ca
Manish Tamang <126256480+Manish-Tamang@users.noreply.github.com>
2024-05-25 23:31:47 +0545

    SEO

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import ImagesList from './components/imagesList/ImagesList';
 import Nav from './components/Nav';
-import Upload from './components/upload/Upload';
 import { Container } from '@mui/material';
 import AuthContext from './context/AuthContext';
 import Modal from './components/Modal';
 import MainNotification from './components/MainNotification';
 import Loading from './components/Loading';
 import Verification from './components/user/Verification';
+
+export const metadata = {
+  title: 'Prashanti Academy SEE Batch 2080',
+  description: 'A simple collection of images of SEE Batch 2080 students from Prashanti Academy.',
+}
 
 function App() {
   return (
@@ -17,7 +21,6 @@ function App() {
         <Verification />
         <MainNotification />
         <Nav />
-        <Upload />
         <ImagesList />
       </AuthContext>
     </Container>
